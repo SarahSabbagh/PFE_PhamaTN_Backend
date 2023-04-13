df626f9be8c2c651b699d0681242f1d4df4a4815
Nour_elhak_benmohamed <nbenmahmoud@anypli.com>
2023-04-13 12:54:58 +0100

    Revert ".env edited online with Bitbucket"
    This reverts commit a5e0a577c7771daaf5d395f84883ce7ed37993e7.

--- src/routes/Initial.tsx
+++ src/routes/Initial.tsx
@@ -3,6 +3,7 @@ import { createBrowserRouter, Navigate, useLoaderData } from "react-router-dom";
 import { SignIn } from "../pages/Login";
 import { Register } from "../pages/Register";
 import { ErrorPage } from "../pages/Error";
+// you should change the name of your routing file 
 export const router = createBrowserRouter([
   {
     path: "/",
