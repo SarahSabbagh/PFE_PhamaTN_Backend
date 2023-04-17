3582a4bbe0e7176ef36c4da2bd31ca0eb2dc7e9f
sarra-sabbagh <sarra.sabbagh@isitc.u-sousse.tn>
2023-04-17 14:59:32 +0200

    PHARMA-14:Solve conflit

--- src/layouts/navbar/Navbar.tsx
+++ src/layouts/navbar/Navbar.tsx
@@ -1,14 +1,13 @@
 import * as React from "react";
 import Toolbar from "@mui/material/Toolbar";
-import { Grid, Typography } from "@mui/material";
+import { Grid } from "@mui/material";
 import { LogoNavbar } from "./logoNavbar/LogoNavbar";
 import { StyledAppBar } from "./Navbar.style";
 import { IconMenu } from "./iconMenu/IconMenu";
 import { MenuList } from "./navMenu/NavMenu";
 import { UserMenu } from "./userMenu/UserMenu";
 import { useAccessToken } from "../../hooks/authHooks";
-import NotificationsIcon from "@mui/icons-material/Notifications";
-import LanguageOutlinedIcon from "@mui/icons-material/LanguageOutlined";
+
 const pagesAutnentication = [
   { route: "/login", name: "Login" },
   { route: "/register", name: "Register" },
@@ -48,7 +47,6 @@ const ResponsiveAppBar: React.FC = () => {
           {!isAuthenticated && (
             <>
               <LogoNavbar isNotAuthenticated />
-
               <MenuList pages={pagesAutnentication} isAuthenticated />
             </>
           )}
@@ -56,64 +54,19 @@ const ResponsiveAppBar: React.FC = () => {
           {isAuthenticated && (
             <>
               <LogoNavbar />
-              {/* <IconMenu
+              <IconMenu
                 pages={pages}
                 anchorEl={anchorElNav}
                 handleClose={handleCloseNavMenu}
                 handleOpen={handleOpenNavMenu}
-          />*/}
+              />
               <LogoNavbar logoxs />
-              {/* <MenuList
+              <MenuList
                 anchorEl={anchorElNav}
                 handleClose={handleCloseNavMenu}
                 handleOpen={handleOpenNavMenu}
                 pages={pages}
-          />*/}
-              <Grid
-                sx={{
-                  position: "relative",
-                  marginRight: "1rem",
-                  cursor: "pointer",
-                }}
-              >
-                <LanguageOutlinedIcon color="primary" />
-              </Grid>
-              <Grid
-                sx={{
-                  position: "relative",
-                  marginRight: "1rem",
-                  cursor: "pointer",
-                }}
-              >
-                <NotificationsIcon color="primary" />
-                <Grid
-                  item
-                  sx={{
-                    position: "absolute",
-                    top: "-.3rem",
-                    right: ".7rem",
-                  }}
-                >
-                  <Typography
-                    color={"white"}
-                    sx={{
-                      height: 15,
-                      width: 15,
-                      position: "absolute",
-                      fontSize: ".7rem",
-                      fontWeight: "700",
-                      bgcolor: "red",
-                      borderRadius: "50%",
-                      display: " flex",
-                      alignItems: "center",
-                      justifyContent: "center",
-                    }}
-                    component={"span"}
-                  >
-                    2
-                  </Typography>
-                </Grid>
-              </Grid>
+              />
               <UserMenu
                 anchorEl={anchorElUser}
                 handleClose={handleCloseUserMenu}
